7f067278fd0cb26621dead39be9e1ed63d4adcd4
Ul Sovanndy <ulsovanndy@Brown-eye.local>
2015-05-30 12:57:48 +0700

    fdfsdfds

---
+++ Demobitbucket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demobitbucket
+//
+//  Created by Ul Sovanndy on 30/5/2015.
+//  Copyright (c) 2015 Ul Sovanndy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
